1fa7c6e31ba6ac5eb0885496777ce6abf4be9d3b
Max <maxkajiwara@gmail.com>
2018-11-08 19:41:28 -0800

    hgfghs

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,7 +4,7 @@ const knexConfig = require('../knexfile');
 require('dotenv').config();
 
 // module.exports = knex(knexConfig.sqlite);
-module.exports = knex(knexConfig[process.env.NODE_ENV || development]);
+module.exports = knex(knexConfig[process.env.DB_ENV || 'development']);
 
 //added HEROKU POSGRESQL DB for testing
 //module.exports = knex(knexConfig.heroku);
